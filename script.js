9a3357f7d1ebbf65938a52ddf0a53a65d52f232d
goninski <webadmin@goninski.ch>
2025-02-10 00:51:08 +0100

    styling comments, save like to storage (unstable)

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+let bookTitleLiked = '';
+let bookTitlesLiked = [];
+let storageStr = '';
+
 function renderBody() {
     renderBooks();
 }
 
 function renderBooks() {
+    getLikedBooksFromStorage();
     let booksListingRef = '';
     let bookCommentsListingRef = '';
     booksListingRef = document.getElementById('booksListing');
@@ -15,3 +20,43 @@ function renderBooks() {
             }
     }
 }
+
+function getLikedBooksFromStorage() {
+    storageStr = localStorage.getItem("bookTitlesLiked");
+    bookTitlesLiked = JSON.parse(storageStr);
+    if(bookTitlesLiked && bookTitlesLiked.length > 0) {
+        for (let index = 0; index < bookTitlesLiked.length; index++) {
+            bookIndex = books.findIndex(element => {return element.name == bookTitlesLiked});
+            books[bookIndex].liked = true;
+        }
+    }
+}
+
+function toggleLikeStatus(bookIndex) {
+    bookLikeStatus = books[bookIndex].liked;
+    if(bookLikeStatus) {
+        bookLikeStatus = false;
+    } else {
+        bookLikeStatus = true;
+    }
+    books[bookIndex].liked = bookLikeStatus;
+    saveLikedBookToStorage(bookIndex);
+    let imgSource = 'assets/icons/favorite-' + bookLikeStatus + '.svg';
+    document.getElementById('bookLikeIcon-' + bookIndex).src = imgSource;
+}
+
+function saveLikedBookToStorage(bookIndex) {
+    bookLikeStatus = books[bookIndex].liked;
+    bookTitleLiked = books[bookIndex].name;
+    let bookIncluded = false;
+    if(bookTitlesLiked) {
+        bookIncluded = bookTitlesLiked.includes(bookTitleLiked);
+    }
+    if(bookLikeStatus && ! bookIncluded) {
+        bookTitlesLiked.push(bookTitleLiked);
+    } else if (! bookLikeStatus && bookIncluded) {
+        bookTitlesLiked = bookTitlesLiked.filter((element) => {return element[bookTitleLiked] == false});
+    }
+    localStorage.setItem("bookTitlesLiked", JSON.stringify(bookTitlesLiked));
+}
+
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,27 +1,3 @@
-function getIcon(type) {
-
-    switch (type) {
-        case 'favorite':
-            return `
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg>
-            `;
-
-    }
-    
-}
-
-let bookIndex = 0;
-let bookTitle = '';
-let bookAuthor = '';
-let bookPrice = 0;
-let bookPublishedYear = 0;
-let bookGenre = '';
-let bookLikes = 0;
-let bookLiked = false;
-let bookComments = [];
-let bookCommentName = '';
-let bookComment = '';
-
 function getBookItemTemplate(bookIndex) {
     bookTitle = books[bookIndex].name;
     bookAuthor = books[bookIndex].author;
@@ -29,21 +5,19 @@ function getBookItemTemplate(bookIndex) {
     bookPublishedYear = books[bookIndex].publishedYear;
     bookGenre = books[bookIndex].genre;
     bookLikes = books[bookIndex].likes;
-    bookLiked = books[bookIndex].liked;
+    bookLikeStatus = books[bookIndex].liked;
     return `
     <div class="book-item flex-col gap">
         <div class="book-header flex-col gap">
-            <h3>${bookTitle}</h3>
-            <img src="assets/img-books/book-sample.jpg" alt="" class="book-cover">
-            <hr>
+            <img src="assets/img/open-book-illustration-PB4109187-red-512.png" alt="open-book-illustration" class="book-cover mt">
+            <h3 class="text-center">${bookTitle}</h3>
         </div>
+        <hr>
         <div class="book-metas">
             <div class="book-meta-header flex-row gap justify-between mb">
                 <div class="book-price">${bookPrice} €</div>
                 <div class="book-likes-wrapper">${bookLikes}
-                    <a class="book-like" onclick="toggleLikeStatus()" href="#" title="mag ich / mag ich nicht">
-                        <img src="assets/icons/favorite-g-red.svg" alt="heart-icon" class="book-like-icon">
-                    </a>
+                    <img src="assets/icons/favorite-${bookLikeStatus}.svg" alt="heart-icon" id="bookLikeIcon-${bookIndex}" class="book-like-icon" onclick="toggleLikeStatus(${bookIndex})">
                 </div>
             </div>
             <table>
@@ -60,10 +34,10 @@ function getBookItemTemplate(bookIndex) {
                     <td>${bookGenre}</td>
                 </tr>
             </table>
-            <hr class="mt">
         </div>
-        <div class="book-comments">
-            <h4 class="mb-05">Kommentare :</h4>
+        <hr>
+        <div class="book-comments flex-col flex-grow justify-between">
+            <h4 class="mb-05">Kommentare </h4>
             <div class="book-comments-listing-wrapper">
                 <table id="bookCommentsListing-${bookIndex}"></table>
             </div>
@@ -82,8 +56,17 @@ function getBookCommentsTemplate(bookIndex, bookComments, commentIndex) {
     bookComment = bookComments[commentIndex].comment;
     return `
     <tr>
-    <td>${bookCommentName}&nbsp;:</td>                                
+    <td>[${bookCommentName}]&nbsp;:</td>                                
     <td>${bookComment}</td>
     </tr>
     `;
+}
+
+function getIcon(type) {
+    switch (type) {
+        case 'like':
+            return `
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg>
+            `;
+    }
 }
