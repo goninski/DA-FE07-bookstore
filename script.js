5b8e1f062d9bf3c4c75388f9c7af37d94b611d9b
goninski <webadmin@goninski.ch>
2025-02-23 09:43:03 +0100

    fixes home url

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ let bookTitlesLiked = [];
 
 function init() {
     renderBooks();
-    setHomeURL();
 }
 
 function renderBooks() {
@@ -120,23 +119,4 @@ function saveBookLikesToStorage(bookIndex, bookLikeStatus) {
         }
     }
     localStorage.setItem("bookTitlesLiked", JSON.stringify(bookTitlesLiked));
-}
-
-function setHomeURL() {
-    let url = new URL(window.location.href);
-    let homeURL = setCondHomeURL(url);
-    let homeLinksRef = document.querySelectorAll('.js-set-home-url');
-    for (let i=0; i < homeLinksRef.length; i++) {
-        homeLinksRef[i].href = homeURL;
-    }
-}
-
-function setCondHomeURL(url) {
-    if(url.hostname == "127.0.0.1") {
-        return url.origin + '/' + url.pathname.split("/")[1];
-    }
-    if(url.pathname.split("/")[1] == "projekte") {
-        return url.origin + '/' + url.pathname.split("/")[1] + '/' + url.pathname.split("/")[2];
-    }
-    return '/';
 }
